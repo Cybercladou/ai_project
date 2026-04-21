cbc37062c2b1f006160caf57e36ab60c50ac9c70
Cybercladou <39793352+Cybercladou@users.noreply.github.com>
2026-04-21 06:46:28 +0000

    Remove RP_ID

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,6 @@ import os
 # For local dev: "localhost"
 # For production: "yourdomain.com"
 # RP_ID = os.getenv("RP_ID", "localhost")
-# RP_ID = os.getenv("RP_ID", "mower-return-wand.ngrok-free.dev")
 RP_ID = os.getenv("RP_ID", "cybercladou.github.io")
 # RP_ID = os.getenv("RP_ID", "bug-free-enigma-w9qpv976g5729pv7-8000.app.github.dev")
 RP_NAME = os.getenv("RP_NAME", "台中旅遊愛好者協會")
@@ -14,7 +13,6 @@ RP_NAME = os.getenv("RP_NAME", "台中旅遊愛好者協會")
 # For local dev: "http://localhost:8000"
 # For production: "https://yourdomain.com"
 # ORIGIN = os.getenv("ORIGIN", "http://localhost:8000")
-# ORIGIN = os.getenv("ORIGIN", "https://mower-return-wand.ngrok-free.dev")
 ORIGIN = os.getenv("ORIGIN", "https://cybercladou.github.io")
 # ORIGIN = os.getenv("ORIGIN", "https://bug-free-enigma-w9qpv976g5729pv7-8000.app.github.dev")
 
--- backend/main.py
+++ backend/main.py
@@ -33,12 +33,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",
         "http://127.0.0.1:8000",
-        "https://mower-return-wand.ngrok-free.dev:8000",
         "https://bug-free-enigma-w9qpv976g5729pv7-8000.app.github.dev",
         "https://cybercladou.github.io",
         "http://localhost:5500",   # VS Code Live Server
         "http://127.0.0.1:5500",
-        "https://mower-return-wand.ngrok-free.dev:5500"
     ],
     allow_credentials=True,
     allow_methods=["*"],
